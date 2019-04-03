8db69abaa0df6c490b2afb7d51da9d20d18f56d8
bksun <sunilsinghmbm@gmail.com>
2019-04-03 10:02:04 +0530

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import { Link, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import GamesPage from "./components/GamesPage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Link to="games">Games</Link>
-          </a>
-        </header>
+        <Link to="games">Games</Link>
+        <Route path="/games" component={GamesPage} />
       </div>
     );
   }
---
+++ src/components/GamesPage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class GamesPage extends Component {
+
+constructor(props) {
+    super(props)
+  }
+
+render() {
+  
+    return (
+        <div>
+            <h2>Games List</h2>
+        </div>
+    )
+  }
+}
