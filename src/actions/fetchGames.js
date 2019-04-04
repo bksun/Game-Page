cb4649960631c142ee7c2092d4778f96f6305a17
bksun <sunilsinghmbm@gmail.com>
2019-04-04 10:06:10 +0530

    set up redux and fetching games data using API http://www.mocky.io/v2/5ca587d0330000f1372ea780

--- src/actions/fetchGames.js
+++ src/actions/fetchGames.js
@@ -1,5 +1,20 @@
+export const SET_GAMES = 'SET_GAMES';
+
+export function setGames(games){
+    // console.log(games);
+
+    return {
+        type: SET_GAMES,
+        games
+    }
+}
+
 export function fetchGames() {
     return dispatch => { //call a function by passing a dispatch function to it.
-        fetch('http://www.mocky.io/v2/5ca56b4733000090322ea6e6')
+        // alert('fetching data from api');
+        fetch('http://www.mocky.io/v2/5ca587d0330000f1372ea780')
+        .then(res => res.json())
+        // .then(res => {console.log(res)})
+        .then(data => dispatch(setGames(data)));
     }
 }
--- src/reducers/games.js
+++ src/reducers/games.js
@@ -1,6 +1,10 @@
+import {SET_GAMES} from '../actions/fetchGames';
 export default function games(state = [], action = {}) {
     
     switch(action.type) {
+        case SET_GAMES:
+            return action.games;
+
         default: return state;
     }
 }
