45eccc97655dd302c281e6b9b8a19120505f55f0
bksun <sunilsinghmbm@gmail.com>
2019-04-06 13:18:37 +0530

    Add form-validation and errors

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,32 +1,62 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
+
 export class GameForm extends Component {
     state = {
         title: '',
-        cover: ''
+        cover: '',
+        errors: {}
     }
 
     handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        });
+        if (!!this.state.errors[e.target.name]) {
+            let errors = Object.assign({}, this.state.errors);
+            delete errors[e.target.name];
+            this.setState({
+                [e.target.name]: e.target.value,
+                errors
+            });
+        }
+        else {
+            this.setState({
+                [e.target.name]: e.target.value
+            });
+
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        //validation
+        let errors = {};
+        if (this.state.title === '') {
+            errors.title = "Can't be empty";
+        }
+        if (this.state.cover === '') {
+            errors.cover = "Can't be empty";
+        }
+
+        this.setState({ errors });
     }
 
     render() {
         return (
 
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div class="ui form">
                     <h2>Add new game</h2>
 
-                    <div className="feild">
+                    <div className={classnames('feild', { error: !!this.state.errors.title })} >
                         <label htmlFor="title">Title</label>
                         <input
                             name="title"
                             value={this.state.title}
                             onChange={this.handleChange}
                             type="text" id="title" />
+                        <span>{this.state.errors.title}</span>
                     </div>
-                    <div className="feild">
+                    <div className={classnames('feild', { error: !!this.state.errors.cover })} >
                         <label htmlFor="cover">Cover URL</label>
                         <input type="text"
                             name="cover"
@@ -34,9 +64,10 @@ export class GameForm extends Component {
                             onChange={this.handleChange}
 
                             id="cover" />
+                        <span>{this.state.errors.cover}</span>
                     </div>
                     <div className="feild">
-                     {this.state.cover !== '' && <img src={this.state.cover} alt="cover" className="ui small bordered image" />}
+                        {this.state.cover !== '' && <img src={this.state.cover} alt="cover" className="ui small bordered image" />}
                     </div>
 
                     <div className="feild">
