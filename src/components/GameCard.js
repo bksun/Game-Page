347e41992d1ac8fc953bae5a2bd194c7aa72697a
bksun <sunilsinghmbm@gmail.com>
2019-04-21 00:27:13 +0530

    Re-Fixed edit page by changing the path to edit the game

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -17,7 +17,7 @@ export default function GameCard({ game, deleteGame }) {
 
             <div className="extra content">
                 <div className="ui two button">
-                    <Link to={`/games/${game._id}`} className="ui basic button green">Edit</Link>
+                    <Link to={`/games/doc/${game._id}`} className="ui basic button green">Edit</Link>
                     <Link className="ui basic button red" onClick={() => deleteGame(game._id)}>Delete</Link>
                 </div>
             </div>
